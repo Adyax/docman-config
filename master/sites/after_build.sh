2928c5fa1f13d7f202d5a0d8934ef88ea9f9caa3
Aleksey Tkachenko <atkachenko@adyax.com>
2014-10-02 12:21:23 +0200

    Update

--- master/sites/after_build.sh
+++ master/sites/after_build.sh
@@ -2,7 +2,4 @@
 
 PREFIX="<?php"
 ENV_PREFIX='$ENVIRONMENT'
-echo "$PREFIX" > sg_master/env.settings.php
-echo "$ENV_PREFIX='$1';" >> sg_master/env.settings.php
-cp default/local.settings.default.php sg_master/local.settings.php
-
+ln -s ../common all
